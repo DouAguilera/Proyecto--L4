d15f6d33a8bb67a634b47ebd4c4f4dae47ae0624
Douglas Aguilera <diaguileram@unah.hn>
2021-11-14 13:20:46 -0600

    Actualizando Templates del Proyecto

--- Electronica/Electronica.Web/Controllers/ComprarController.cs
+++ Electronica/Electronica.Web/Controllers/ComprarController.cs
@@ -1,19 +1,23 @@
 ﻿using Electronica.BL;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Electronica.Web.Controllers
 {
-    public class ProductosController : Controller
+    public class ComprarController : Controller
     {
-        // GET: Productos
+        // GET: Comprar
         public ActionResult Index()
         {
             var productosBL = new ProductosBL();
-            var ListadeProductos = productosBL.ObtenerProductos();
+            var ListadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl
+                = ConfigurationManager.AppSettings["adminWebsiteUrl"];
 
             return View(ListadeProductos);
         }
